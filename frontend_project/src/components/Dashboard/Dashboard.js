e658bfef0cc40ed94dbac60f56f1d7c3a7f3a023
akhilkrishnamurthy <akilkrishnamurthy@gmail.com>
2020-03-18 13:55:16 -0700

    Adding bar chart for land value

--- frontend_project/src/components/Dashboard/Dashboard.js
+++ frontend_project/src/components/Dashboard/Dashboard.js
@@ -15,7 +15,7 @@ class Dashboard extends Component {
                 labels: ['Arizona', 'Boston', 'Cali', 'Denver', 'Detroitte', 'New York'],
                 datasets: [
                     {
-                        label: 'population',
+                        label: 'Land Value',
                         data:[
                             12345,
                             54635,
@@ -49,12 +49,31 @@ class Dashboard extends Component {
                 console.log("response from  server");
                 console.log(result.data);
                 let labels = [];
+                let data = [];
                 for(let i=0;i<result.data.alllanddata.length;i++) {
                     labels.push(result.data.alllanddata[i].Date);
+                    data.push(result.data.alllanddata[i].LandValue);
                 }
+                console.log(labels, data);
                 this.setState({
                     placeData: result.data.alllanddata,
-                    // chartData: 
+                    chartData: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: 'Land Value',
+                                data: data,
+                                backgroundColor: [
+                                    'rgba(255, 99, 123, 0.6)',
+                                    'rgba(54, 162, 235, 0.6)',
+                                    'rgba(255, 206, 86, 0.6)',
+                                    'rgba(75, 192, 192, 0.6)',
+                                    'rgba(153, 102, 255, 0.6)',
+                                    'rgba(255, 159, 64, 0.6)'
+                                ]
+                            }
+                        ]
+                    }
                 });
             }).catch((error)=>{
                 console.log("error");
