17c8ff7e2cf31ccede5cb729665a57454586e968
SayaliPatil <saypatil12345@gmail.com>
2020-04-17 15:40:45 -0700

    adding code for colored graphs

--- frontend_project/src/components/Dashboard/Dashboard.js
+++ frontend_project/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import './Dashboard.css';
 import { api } from './../../constants';
 import { Bar, Line, Pie } from 'react-chartjs-2';
+
 import { Spinner, UncontrolledAlert } from 'reactstrap';
 
 class Dashboard extends Component {
@@ -67,9 +70,21 @@ class Dashboard extends Component {
                 structureCostData.push(result.data.alllanddata[i].StructureCost);
             }
 
-            console.log(labels, data);
-            console.log(homeData);
-            console.log("structure cost data " + structureCostData);
+            console.log("LABELS : " + labels);
+            let results = [];
+            let iterator = labels.values();
+            for (let elements of iterator) { 
+                console.log(elements);
+                if(elements.substring(4, 6) == 'Q1')
+                results.push('rgba(255, 99, 123, 0.6)');
+                else if(elements.substring(4, 6) == 'Q2')
+                results.push('rgba(54, 162, 235, 0.6)');
+                else if(elements.substring(4, 6) == 'Q3')
+                results.push('rgba(255, 206, 86, 0.6)');
+                else if(elements.substring(4, 6) == 'Q4')
+                results.push('rgba(75, 192, 192, 0.6)');
+            }
+            console.log("RESULTS : ", results);
 
             this.setState({
                 loading:false,
@@ -80,14 +95,7 @@ class Dashboard extends Component {
                         {
                             label: 'Land Value',
                             data: data,
-                            backgroundColor: [
-                                'rgba(255, 99, 123, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(255, 206, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(255, 159, 64, 0.6)'
-                            ]
+                            backgroundColor: results
                         }
                     ]
                 },
@@ -97,14 +105,7 @@ class Dashboard extends Component {
                         {
                             label: 'Home Value',
                             data: homeData,
-                            backgroundColor: [
-                                'rgba(255, 99, 123, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(255, 206, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(255, 159, 64, 0.6)'
-                            ]
+                            backgroundColor: results
                         }
                     ]
                 },
@@ -114,14 +115,7 @@ class Dashboard extends Component {
                         {
                             label: 'Structure Cost Value',
                             data: structureCostData,
-                            backgroundColor: [
-                                'rgba(255, 99, 123, 0.6)',
-                                'rgba(54, 162, 235, 0.6)',
-                                'rgba(255, 206, 86, 0.6)',
-                                'rgba(75, 192, 192, 0.6)',
-                                'rgba(153, 102, 255, 0.6)',
-                                'rgba(255, 159, 64, 0.6)'
-                            ]
+                            backgroundColor: results
                         }
                     ]
                 }
@@ -144,46 +138,68 @@ class Dashboard extends Component {
 
         if(!loading){
             dashboard = (
-                <>
-                <div className="wrapper row">
-                    <h2 className="header">Dashboard Charts</h2>
-
-                    <div>Land Value</div>
-                    <div className="col-md-12 barchart" style={{ padding: '5px' }}>
-                        <Bar data={this.state.chartData}
-                            options={{
-                            }} 
-                        />
-                    </div>
-
-
-                </div>
-
-                <div className="row">
-                    <div>Home Value</div>
-                    <div className="col-md-12">
-                        <Bar data={this.state.homeChartData}
-                            options={{
-                            }}
-                        />
-                    </div>
-
-
-                </div>
-
-                <div className="row">
-
-                <div>Structure Value</div>
-                    <div className="col-md-12">
-                        <Bar data={this.state.costChartData}
-                            options={{
-                            }}
-                        />
-                    </div>
-
-                </div>
-
-            </>
+                
+            //     <div className="wrapper row">
+            //         <h2 className="header">Dashboard Charts</h2>
+            //         {/* <h3>Land Value</h3> */}
+            //         <div className="col-md-6 barchart">
+            //             <Bar data={this.state.chartData}
+            //                 options={{
+            //                 }} 
+            //             />
+            //         </div>
+            //         {/* <div>Home Value</div> */}
+            //         <div className="col-md-6">
+            //             <Bar data={this.state.homeChartData}
+            //                 options={{
+            //                 }}
+            //             />
+            //         </div>
+            //         {/* <div>Structure Value</div> */}
+            //         <div className="col-md-6">
+            //             <Bar data={this.state.costChartData}
+            //                 options={{
+            //                 }}
+            //             />
+            //         </div>
+            //    </div>
+            <div className="outer">
+            <Tabs defaultIndex={0}>
+                <TabList>
+                    <Tab>Land Value</Tab>
+                    <Tab>Home Value</Tab>
+                    <Tab>Structure Value</Tab>
+                </TabList>
+               
+                <TabPanel>
+                <div className="charts">
+                <Bar data={this.state.chartData}
+                    options={{
+                        responsive : true,
+                        maintainAspectRatio: false
+                    }} 
+                /></div>
+                </TabPanel>
+                <TabPanel>
+                <div className="charts">
+                <Bar data={this.state.homeChartData}
+                    options={{
+                        responsive : true,
+                        maintainAspectRatio: false
+                    }} 
+                /></div>
+                </TabPanel>
+                <TabPanel>
+                <div className="charts">
+                <Bar data={this.state.costChartData}
+                    options={{
+                        responsive : true,
+                        maintainAspectRatio: false
+                    }} 
+                /></div>
+                </TabPanel>
+            </Tabs>
+            </div>
             );
         }else{
             dashboard = (
